bb5b98d17926f1868377d1f319ef4dfbd5527577
Karl Kundla <karl.kundla@tptlive.ee>
2025-09-22 15:24:18 +0300

    Midagi

---
+++ index.js
@@ -0,0 +1,35 @@
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);    
+let text = `Hello World`;
+console.log(text);
+console.log(typeof text);
+text = `D`; // char
+console.log(text);
+console.log(typeof text);
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+let array = [1, 2, `hello`, `world`, true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+let obj = {
+    name: `Karl`,
+    age: 17,
+    hairColor: `Brown`,
+    isMale: true,
+}
+console.log(obj);
+console.log(typeof obj);
