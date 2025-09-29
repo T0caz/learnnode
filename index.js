98e7d6882708b0c656077d3ecf68a1c461a22ff1
Karl Kundla <karl.kundla@tptlive.ee>
2025-09-29 14:42:26 +0300

    aeg?

--- index.js
+++ index.js
@@ -1,55 +1,24 @@
-function hello() {
-    console.log('Hello World');
+let time = Date.now();
+let num = 0;
+while(Date.now() < time+1000) {
+    num++;
+}
+console.log(num);
+
+num = 10 ;
+while(num < 10) {
+    console.log('WHILE');
 }
 
-hello();
-hello();
+do {
+    console.log ('DO');
+} while(num < 10);
 
-function helloName(name) {
-    console.log(`Hello ${name}!`)
-}
-
-helloName('Karl');
-helloName('Kaka');
-
-function hi(name='Nameless', age='Infinite') {
-    console.log(`Hi ${name} you are ${age} year old`);
-}
-hi('Karl', 17);
-hi('Kaka', 67);
-
-let sum = function (a, b) {
-    return a+b;
-}
-
-let answer = sum(4, 5);
-console.log(answer);
-console.log(sum(6, 10));
-sum(5, 6);
-let sub = (a, b) => {
-    return a-b;
-}
-
-console.log(sub(6, 10));
- 
-let mult = (a, b) => a*b;
-
- console.log(mult(6, 10));
-
- let root = a => a*a;
-
- console.log(root(6));
-
- let cool = () => console.log('cool');
-
- cool();
-
- let awesome = {
-    party: function () {
-        console.log('PARTY!');
-    },
-    cool: () => console.log('cool'),
-    nice () {
-        console.log('nice');
+function recursion(num) {
+    if(num<10) {
+        console.log(num);
+        recursion(num+1);
     }
- };
+}
+
+recursion
