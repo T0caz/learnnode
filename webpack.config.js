dadc64ba16048b59ec7d1342ed5e3f2552345d7c
Karl Kundla <karl.kundla@tptlive.ee>
2025-10-06 15:50:42 +0300

    Add nunjucks

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,42 @@ export default {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.s[ca]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader", 
+          "css-loader", 
+          {
+                loader:'sass-loader',
+                options: {
+                    sassOptions: {
+                        quietDeps: true
+                    }
+                }
+            }
+        ],
       },
+       {
+                test: /\.njk$/,
+                use: [
+                    {
+                        loader: 'simple-nunjucks-loader',
+                        options: {}
+                    }
+                ]
+            }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template:'./src/index.html'
-    })
+        template:'./src/views/index.njk',
+    }),
+    new HtmlWebpackPlugin({
+        filename:'about.html',
+        template:'./src/views/about.njk',
+    }),
+    new HtmlWebpackPlugin({
+      filename:'contacts.html',
+      template:'./src/views/contacts.njk',
+    }),
   ],
 };
