32dcf4ecdda45bc96aae9993bd1ad31355908e2d
Karl Kundla <karl.kundla@tptlive.ee>
2025-10-13 14:44:42 +0300

    eelmine tund lõpp plus 13.10 tund algus

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+let newItem = ref('');
+let i = 1;
+let items = ref([
+    {id: i++, name:'Leib', isDone: false}, 
+    {id: i++, name:'Sai', isDone: false}, 
+    {id: i++, name:'Õlu', isDone: true}, 
+    {id: i++, name:'Viin', isDone: true},
+]);
+let code = ref(404);
+function addItem(){
+    items.value.push({id: i++, name: newItem.value, isDone: false},);
+    newItem.value = '';
+}
+
+</script>
+<template>
+<div class="container">
+    <div class="content">
+        <div class="field has-addons">
+            <div class="control is-expanded">
+                <input v-model="newItem" class="input" type="text" placeholder="Add item">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <h1>{{ newItem }}</h1>
+        <ul>
+            <li v-for="item in items">{{ item }}</li>
+        </ul>
+        <input v-model="code" class="input" type="number" placeholder="Enter Error code">
+        <img :src="'https://http.cat/' + code">
+    </div>
+</div>
+</template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-
-
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { VueLoaderPlugin } from "vue-loader";
+import webpack from "webpack";
 
 export default {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(import.meta.dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
-   devServer: {
+  devServer: {
     static: {
-      directory: path.join(import.meta.dirname, 'public'),
+      directory: path.join(import.meta.dirname, "public"),
     },
     compress: true,
     port: 9000,
@@ -27,23 +26,33 @@ export default {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader", 
-          "css-loader", 
+          "style-loader",
+          "css-loader",
           {
-                loader:'sass-loader',
-                options: {
-                    sassOptions: {
-                        quietDeps: true
-                    }
-                }
-            }
+            loader: "sass-loader",
+            options: {
+              sassOptions: {
+                quietDeps: true,
+              },
+            },
+          },
         ],
-      }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template:'./src/index.html',
+      template: "./src/index.html"
     }),
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: 'true',
+      __VUE_PROD_DEVTOOLS__: 'false',
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+    })
   ],
 };
